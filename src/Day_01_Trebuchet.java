a2d732df4aeee93a2d89dd3813544a51841f2594
Kristjan Vingel <kristjan.vingel.protonmail.ch>
2023-12-02 22:43:00 +0200

    Day 1 complete

--- src/Day_01_Trebuchet.java
+++ src/Day_01_Trebuchet.java
@@ -2,105 +2,74 @@ import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.HashMap;
-import java.util.HashSet;
 import java.util.Map;
-import java.util.Set;
-
 
 public class Day_01_Trebuchet {
     public static void main(String[] args) {
-        String inputPath = "sample.txt";
+        String inputPath = "input.txt";
         int sum = 0;
 
-        Set<String> numbers = new HashSet<>();
-        numbers.add("1");
-        numbers.add("2");
-        numbers.add("3");
-        numbers.add("4");
-        numbers.add("5");
-        numbers.add("6");
-        numbers.add("7");
-        numbers.add("8");
-        numbers.add("9");
-        numbers.add("one");
-        numbers.add("two");
-        numbers.add("three");
-        numbers.add("one");
-        numbers.add("one");
-        numbers.add("one");
-        numbers.add("one");
-
-        Map<String, String> nums = new HashMap<>();
-        nums.put("1", "one");
-        nums.put("2", "two");
-        nums.put("3", "three");
-        nums.put("4", "four");
-        nums.put("5", "five");
-        nums.put("6", "six");
-        nums.put("7", "seven");
-        nums.put("8", "eight");
-        nums.put("9", "nine");
+        Map<String, Character> hashmap = new HashMap<>();
+        hashmap.put("one", '1');
+        hashmap.put("two", '2');
+        hashmap.put("three", '3');
+        hashmap.put("four", '4');
+        hashmap.put("five", '5');
+        hashmap.put("six", '6');
+        hashmap.put("seven", '7');
+        hashmap.put("eight", '8');
+        hashmap.put("nine", '9');
+        hashmap.put("1", '1');
+        hashmap.put("2", '2');
+        hashmap.put("3", '3');
+        hashmap.put("4", '4');
+        hashmap.put("5", '5');
+        hashmap.put("6", '6');
+        hashmap.put("7", '7');
+        hashmap.put("8", '8');
+        hashmap.put("9", '9');
 
         try {
             BufferedReader reader = new BufferedReader(new FileReader(inputPath));
 
             String line;
-            while((line = reader.readLine()) != null) {
+            while ((line = reader.readLine()) != null) {
+                int firstNumIdx = line.length() - 1;
                 char firstNum = '0';
+
+                int secondNumIdx = 0;
                 char secondNum = '0';
 
-                for (int i = 0; i < line.length(); i++) {
-                    char current = line.charAt(i);
+                // find the first number
+                for (String key : hashmap.keySet()) {
+                    int idx = line.indexOf(key);
 
-                    if (numbers.contains(current)) {
-                        firstNum = current;
-                        break;
+                    if (idx != -1 && idx <= firstNumIdx) {
+                        firstNumIdx = idx;
+                        firstNum = hashmap.get(key);
                     }
                 }
 
-                for (int i = line.length() - 1; i >= 0; i--) {
-                    char current = line.charAt(i);
+                // find the second number
+                for (String key : hashmap.keySet()) {
+                    int idx = line.lastIndexOf(key);
 
-                    if (numbers.contains(current)) {
-                        secondNum = current;
-                        break;
+                    if (idx != -1 && idx >= secondNumIdx) {
+                        secondNumIdx = idx;
+                        secondNum = hashmap.get(key);
                     }
                 }
 
-                int lineSum = Integer.parseInt(
-                        Character.toString(firstNum) +
-                                Character.toString(secondNum));
-
+                int lineSum = Integer.parseInt(Character.toString(firstNum) + Character.toString(secondNum));
                 sum += lineSum;
-
             }
 
             reader.close();
 
-        } catch(IOException e) {
+        } catch (IOException e) {
             e.printStackTrace();
         }
 
         System.out.println(sum);
-
     }
 }
-
-//        zoneight234
-
-//        get the number pos
-//        see if you can find a word,
-//        if so, check if its start index is before the number
-//        use the word instead
-
-
-// two1nine
-// eightwothree
-// abcone2threexyz
-// xtwone3four
-// 4nineeightseven2
-// zoneight234
-// 7pqrstsixteen
-//
-// 2 + 9
-// 8 + 3
