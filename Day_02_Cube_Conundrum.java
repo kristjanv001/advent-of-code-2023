fcc401f11c4e9ea967d9bc8df4f7449e6af11ddd
Kristjan Vingel <kristjan.vingel.protonmail.ch>
2023-12-03 19:12:41 +0200

    Day 2 p2 done

--- Day_02_Cube_Conundrum.java
+++ Day_02_Cube_Conundrum.java
@@ -12,7 +12,7 @@ public class Day_02_Cube_Conundrum {
   public static int intFromString(String str) {
     Pattern pattern = Pattern.compile("\\b\\d+\\b");
     Matcher matcher = pattern.matcher(str);
-      
+
     if (matcher.find()) {
       String numericValue = matcher.group();
 
@@ -24,24 +24,19 @@ public class Day_02_Cube_Conundrum {
 
   public static void main(String[] args) {
     String inputPath = "input.txt";
-
-    Map<String, Integer> hashmap = new HashMap<>();
-    hashmap.put("red", 12);
-    hashmap.put("green", 13);
-    hashmap.put("blue", 14);
-    
-    int possibleGames = 0;
-
-    // Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green
+    int sumOfPowers = 0;
 
     try {
       BufferedReader reader = new BufferedReader(new FileReader(inputPath));
 
       String line;
       while ((line = reader.readLine()) != null) {
-        boolean isIdPossible = true;
-        // get the id
-        int id = Integer.parseInt(line.substring(5, line.indexOf(":")));
+
+        // keeps tracks of the max red/green/blue values
+        Map<String, Integer> hashmap = new HashMap<>();
+        hashmap.put("red", 0);
+        hashmap.put("green", 0);
+        hashmap.put("blue", 0);
 
         // create array of parts
         String substring = line.substring(line.indexOf(":") + 2);
@@ -50,28 +45,29 @@ public class Day_02_Cube_Conundrum {
         // check if any values are over the limit
         for (String part : parts) {
           int amount = intFromString(part);
-          
+
           for (String key : hashmap.keySet()) {
             int idx = part.indexOf(key);
 
             if (idx != -1) {
-              if (amount > hashmap.get(key)) {
-                isIdPossible = false;
-                break;
-              }
+              // upgrade the max val in the hashmap
+              hashmap.put(key, Math.max(hashmap.get(key), amount));
             }
           }
         }
 
-        if (isIdPossible) {
-          possibleGames += id;
+        // loop over the hashmap values --> multiply --> add to the sum variable
+        int powers = 1;
+        for (int value : hashmap.values()) {
+          powers *= value;
         }
 
-      }
+        sumOfPowers += powers;
 
+      }
       reader.close();
 
-      System.out.println(possibleGames);
+      System.out.println(sumOfPowers);
 
     } catch (IOException e) {
       e.printStackTrace();
