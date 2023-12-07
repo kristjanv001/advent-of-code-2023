76c0d7f0703561a6989b2362b67d4863193c8934
Kristjan Vingel <kristjan.vingel@protonmail.ch>
2023-12-08 00:14:19 +0200

    Day 4 p2 done

--- Day_04_Scratchcards.java
+++ Day_04_Scratchcards.java
@@ -1,33 +1,55 @@
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
-import java.util.Arrays;
-import java.util.HashSet;
+import java.util.Map;
+import java.util.HashMap;
 import java.util.Set;
-
+import java.util.HashSet;
 
 public class Day_04_Scratchcards {
   String inputPath;
-
+  Map<Integer, Integer> cardMatches;
+  
   Day_04_Scratchcards(String input) {
     this.inputPath = input;
+    this.cardMatches =  mapCardIdToMatches();
   }
 
-  public void getTotalPoints() {
-    int total = 0;
+  public void getTotalCards() {
+    Map<Integer, Integer> totalCards = new HashMap<>();
+
+    for (int cardId : cardMatches.keySet()) {
+      totalCards.put(cardId, 1);
+    }
+
+    for (int i = 1; i <= cardMatches.size(); i++) {
+      for (int j = i + 1; j <= i + cardMatches.get(i); j++) {
+        totalCards.put(j, totalCards.get(j) + totalCards.get(i));
+      }
+    }
+
+    int sum = totalCards.values().stream().mapToInt(value -> value.intValue()).sum();
+    System.out.println(sum);
+  }
+
+  private Map<Integer, Integer> mapCardIdToMatches() {
+    Map<Integer, Integer> map = new HashMap<>();
 
     try {
       BufferedReader reader = new BufferedReader(new FileReader(this.inputPath));
-      
+
       String line;
+      int cardNumber = 1;
       while ((line = reader.readLine()) != null) {
         String[] numbers = line.split(":")[1].split("\\|");
         Set<Integer> winningNumbers = parseNumbers(numbers[0]);
         Set<Integer> myNumbers = parseNumbers(numbers[1]);
 
-        Set<Integer> matchingNumbers = getMatchingNUmbers(winningNumbers, myNumbers);
+        Set<Integer> matchingNumbers = getMatchingNumbers(winningNumbers, myNumbers);
 
-        total += getCardPoints(matchingNumbers);
+        map.put(cardNumber, matchingNumbers.size());
+        cardNumber++;
+
       }
 
       reader.close();
@@ -36,14 +58,10 @@ public class Day_04_Scratchcards {
       e.printStackTrace();
     }
 
-    System.out.println(total);
+    return map;
   }
 
-  private int getCardPoints(Set<Integer> matchingNums) {
-    return (int)Math.pow(2, matchingNums.size() - 1);
-  }
-
-  private Set<Integer> getMatchingNUmbers(Set<Integer> winning, Set<Integer> myNums) {
+  private Set<Integer> getMatchingNumbers(Set<Integer> winning, Set<Integer> myNums) {
     Set<Integer> matchingNumbers = new HashSet<>(winning);
     matchingNumbers.retainAll(myNums);
 
@@ -54,11 +72,11 @@ public class Day_04_Scratchcards {
     Set<Integer> numbers = new HashSet<>();
 
     for (String number : numbersAsString.trim().split("\\s+")) {
-        numbers.add(Integer.parseInt(number));
+      numbers.add(Integer.parseInt(number));
     }
 
     return numbers;
-}
+  }
 
   public static void main(String[] args) {
     System.out.println("----------------");
@@ -66,7 +84,6 @@ public class Day_04_Scratchcards {
     System.out.println("----------------");
 
     Day_04_Scratchcards scratchcards = new Day_04_Scratchcards("input.txt");
-    scratchcards.getTotalPoints();
-
+    scratchcards.getTotalCards();
   }
 }
